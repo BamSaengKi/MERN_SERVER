c41ac03858b5ac559974df7a8a4052141042f9e3
Bam <bamsaengki@gmail.com>
2022-07-08 00:29:31 +0900

    [UPLOAD]RESTAPI

---
+++ Routes/AuthRoute.js
@@ -0,0 +1,9 @@
+import express  from "express";
+import {LoginUser, registerUser} from "../Controller/AuthController.js"
+
+const router = express.Router()
+
+
+router.post('/register', registerUser)
+router.post('/login', LoginUser)
+export default router
